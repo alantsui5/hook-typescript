54fc1074f9a0804b7cac1a9fb129402ae7cb3897
Alan Tsui <1155110323@link.cuhk.edu.hk>
2020-04-11 21:00:49 +0800

    Add Routing Lazyload

--- src/routing.tsx
+++ src/routing.tsx
@@ -1,10 +1,14 @@
-import User from './features/user/user'
-import App from './App'
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
+const App = lazy(() => import('./App'));
+const User = lazy(() => import('./features/user/user'));
+
+const Loading = () => {
+    return (<>Loading</>)
+}
 
 const routes = {
-    "/": () => <App />,
-    "/user": () => <User />,
+    "/": () => <Suspense fallback={<Loading />}><App /></Suspense>,
+    "/user": () => <Suspense fallback={<Loading />}><User /></Suspense>,
 }
 
 export default routes;
