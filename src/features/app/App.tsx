f9b385448ee13dc57bff93b67fbf9e761365fc2d
Älan <1155110323@link.cuhk.edu.hk>
2020-04-12 02:45:46 +0800

    Modify App tsx

--- src/features/app/App.tsx
+++ src/features/app/App.tsx
@@ -10,7 +10,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
-        <User />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
