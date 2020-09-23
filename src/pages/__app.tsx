d26a08360afe78f891891d670442438978694e68
Alan Tsui Ho Ming <33037271+alantsui5@users.noreply.github.com>
2020-09-23 21:47:25 +0800

    Update __app.tsx

--- src/pages/__app.tsx
+++ src/pages/__app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from 'next/app'
+import Stores, {useGlobal} from '../stores'
 
 import '../styles.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const global = useGlobal();
+  return 
+  <Stores.Provider value={global}>
+    <Component {...pageProps} />
+  </Stores.Provider>
 }
 
 export default MyApp
